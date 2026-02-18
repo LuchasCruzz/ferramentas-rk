aa023dad2a6c9b8d2ca596de11ead56abbbd8ca2
LuchasCruzz <42675226+LuchasCruzz@users.noreply.github.com>
2026-02-18 11:29:38 -0300

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,11 @@ import {setRequestLocale} from 'next-intl/server';
 import type {AppLocale} from '@/i18n/routing';
 import {HomeClient} from '@/components/home/HomeClient';
 
-export default function HomePage({params}: {params: {locale: AppLocale}}) {
-  setRequestLocale(params.locale);
+type Params = {locale: AppLocale};
+type Props = {params: Params | Promise<Params>};
+
+export default async function HomePage({params}: Props) {
+  const {locale} = await Promise.resolve(params);
+  setRequestLocale(locale);
   return <HomeClient />;
 }
